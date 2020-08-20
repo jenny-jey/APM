377091a657ecaffb3d031a8dc3d5e07a3f12efce
jennifer.jeyakodi <jennifer.jeyakodi@start.ie>
2020-08-20 14:23:22 +0100

    productlist

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -1,4 +1,6 @@
 import { Component } from "@angular/core";
+import { IProduct } from "./product";
+
 
 @Component({
     selector:'pm-products',
@@ -8,15 +10,23 @@ import { Component } from "@angular/core";
 
 export class ProductListComponent{
     pageTitle: string="Product List";
-    products : any[] = [
-       {"productId":2,
+    imageWidth : number = 50;
+    imageMargin : Number =2;
+    showImage : boolean = false;
+    listFilter : string = 'Cart';
+    products :  IProduct [] =[
+       {"productId": 2,
     "productName":"Garden Cart",
      "productCode":"GBN-0023",
     "releaseDate":"March 18, 2019",
     "description":"Garden cart",
-    "price":"32.99",
-    "starRating":"4.2",
-    "imageUrl":"assets/Images/Garden-Cart.jpg",
+    "price":32.99,
+    "starRating":4.2,
+    "imageUrl":"assets/Images/Garden-Cart.jpg", 
 }
 ];
+toggleImage(): void{
+    this.showImage = !this.showImage; 
+ }
+
 }
